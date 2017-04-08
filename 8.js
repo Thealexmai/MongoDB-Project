71491c9521837b59c4ce4328757ed42b6b6501c4
Alex <amai2@u.rochester.edu>
2017-04-08 14:03:35 -0400

    Implement Task 8

---
+++ 8.js
@@ -0,0 +1,13 @@
+db.items.insert(
+	{
+		"_id": "88888888",
+		"Name": "Test Task 8",
+		"Category": [
+			"Task 8",
+			"Database Systems"
+		]
+	}
+)
+
+// Test if ran
+// db.items.find({_id: "88888888"})
